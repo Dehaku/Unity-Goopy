518abcfa422f3917d2e55af37247b33cc3ade7e4
Dehaku <dehakuzedon@gmail.com>
2021-03-10 23:42:56 -0600

    Goop Gravity tests

--- Assets/Scripts/GoopyController.cs
+++ Assets/Scripts/GoopyController.cs
@@ -8,6 +8,10 @@ public class GoopyController : MonoBehaviour
     [SerializeField] public float goopSpringDampeningRatio = 0;
     [SerializeField] public float goopSpringFrequency = 1;
     [SerializeField] public float goopFriction = 1;
+    [SerializeField] GameObject centerGoop;
+
+    Vector3 _centerOfGoops;
+    Rigidbody2D[] _goops;
 
     
     // Start is called before the first frame update
@@ -16,15 +20,47 @@ public class GoopyController : MonoBehaviour
         
     }
 
+    void CalcCenterOfGoops()
+    {
+        _goops = GetComponentsInChildren<Rigidbody2D>();
+
+        if (_goops.Length == 0)
+        {
+            _centerOfGoops = new Vector3();
+            return;
+        }
+            
+        foreach (var goop in _goops)
+            _centerOfGoops += goop.transform.position;
+
+        _centerOfGoops /= _goops.Length + 1;
+    }
+
+    void GravityGoops()
+    {
+        foreach (var goop in _goops)
+        {
+            var _rigidbody2D = goop.GetComponent<Rigidbody2D>();
+            var currentPosition = _rigidbody2D.position;
+            Vector2 direction = new Vector2(_centerOfGoops.x,_centerOfGoops.y) - currentPosition;
+            direction.Normalize();
+
+            _rigidbody2D.AddForce(direction * 5);
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        CalcCenterOfGoops();
 
-    private void OnTransformParentChanged()
-    {
-        
+        if (Input.GetKey(KeyCode.Q))
+            GravityGoops();
+
+        if(_goops.Length > 0)
+        {
+            centerGoop.transform.position = _centerOfGoops;
+        }
     }
 
 }
